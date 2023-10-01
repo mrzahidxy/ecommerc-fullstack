257d522e0e1496e4826bf04e0b228134184c46aa
Zahid <mrzahidxy@gmail.com>
2023-10-01 23:07:02 +0600

    verify token

--- server/routes/product.js
+++ server/routes/product.js
@@ -8,6 +8,7 @@ const {
 } = require("firebase/storage");
 const storage = require("../firebase");
 const product = require("../models/product");
+const { verifyToken, verifyTokenAndAdmin } = require("./verifyToken");
 
 const router = express.Router();
 const maxFileSize = 5 * 1024 * 1024; // 5MB
@@ -60,106 +61,116 @@ const uploadImageAndCreateProduct = async (
 };
 
 // Create a new item
-router.post("/", upload.single("image"), async (req, res) => {
-  try {
-    const { title, description, price, quantity, category, color, size } =
-      req.body;
-    const img = req.file;
+router.post(
+  "/",
+  verifyTokenAndAdmin,
+  upload.single("image"),
+  async (req, res) => {
+    try {
+      const { title, description, price, quantity, category, color, size } =
+        req.body;
+      const img = req.file;
 
-    if (!img) {
-      return res.status(400).json({ error: "Image file is required" });
+      if (!img) {
+        return res.status(400).json({ error: "Image file is required" });
+      }
+
+      const createdProduct = await uploadImageAndCreateProduct(
+        img,
+        title,
+        description,
+        price,
+        quantity,
+        category,
+        color,
+        size
+      );
+
+      res.status(201).json(createdProduct);
+    } catch (error) {
+      console.error("Error creating product item:", error);
+      res.status(500).json({ error: "Internal Server Error" });
     }
-
-    const createdProduct = await uploadImageAndCreateProduct(
-      img,
-      title,
-      description,
-      price,
-      quantity,
-      category,
-      color,
-      size
-    );
-
-    res.status(201).json(createdProduct);
-  } catch (error) {
-    console.error("Error creating product item:", error);
-    res.status(500).json({ error: "Internal Server Error" });
   }
-});
+);
 
 // Update an existing product
-router.put("/:id", upload.single("image"), async (req, res) => {
-  try {
-    const { title, description, price, quantity, category, color, size } =
-      req.body;
-    const img = req.file;
-    const productId = req.params.id;
+router.put(
+  "/:id",
+  verifyTokenAndAdmin,
+  upload.single("image"),
+  async (req, res) => {
+    try {
+      const { title, description, price, quantity, category, color, size } =
+        req.body;
+      const img = req.file;
+      const productId = req.params.id;
 
-    // First, delete the old image from Firebase Storage (if it exists)
-    const existingProduct = await product.findById(productId);
-    if (existingProduct) {
-      // Extract the filename from the existing image URL
-      const existingImageRef = ref(
-        storage,
-        getObjectPathFromUrl(existingProduct.img)
-      );
+      // First, delete the old image from Firebase Storage (if it exists)
+      const existingProduct = await product.findById(productId);
+      if (existingProduct) {
+        // Extract the filename from the existing image URL
+        const existingImageRef = ref(
+          storage,
+          getObjectPathFromUrl(existingProduct.img)
+        );
 
-      // Delete the existing image
-      try {
-        await deleteObject(existingImageRef);
-      } catch (error) {
-        console.error("Error deleting existing image:", error);
+        // Delete the existing image
+        try {
+          await deleteObject(existingImageRef);
+        } catch (error) {
+          console.error("Error deleting existing image:", error);
+        }
       }
+
+      // Upload the new image to Firebase Storage
+      if (img) {
+        const filename = `${Date.now()}_${img.originalname}`;
+        const fileRef = ref(storage, filename);
+        await uploadBytes(fileRef, img.buffer);
+        const imageUrl = await getDownloadURL(fileRef);
+
+        // Update the portfolio item in the database with the new image URL
+        const updatedProduct = await product.findByIdAndUpdate(
+          productId,
+          {
+            title,
+            description,
+            price,
+            quantity,
+            category,
+            color,
+            size,
+            img: imageUrl,
+          },
+          { new: true }
+        );
+
+        res.status(200).json(updatedProduct);
+      } else {
+        // If no new image is provided, update the portfolio item without changing the image
+        const updatedProduct = await Portfolio.findByIdAndUpdate(
+          portfolioItemId,
+          {
+            title,
+            description,
+            price,
+            quantity,
+            category,
+            color,
+            size,
+          },
+          { new: true }
+        );
+
+        res.status(200).json(updatedProduct);
+      }
+    } catch (error) {
+      console.error("Error updating portfolio item:", error);
+      res.status(500).json({ error: "Internal Server Error" });
     }
-
-    // Upload the new image to Firebase Storage
-    if (img) {
-      const filename = `${Date.now()}_${img.originalname}`;
-      const fileRef = ref(storage, filename);
-      await uploadBytes(fileRef, img.buffer);
-      const imageUrl = await getDownloadURL(fileRef);
-
-      // Update the portfolio item in the database with the new image URL
-      const updatedProduct = await product.findByIdAndUpdate(
-        productId,
-        {
-          title,
-          description,
-          price,
-          quantity,
-          category,
-          color,
-          size,
-          img: imageUrl,
-        },
-        { new: true }
-      );
-
-      res.status(200).json(updatedProduct);
-    } else {
-      // If no new image is provided, update the portfolio item without changing the image
-      const updatedProduct = await Portfolio.findByIdAndUpdate(
-        portfolioItemId,
-        {
-          title,
-          description,
-          price,
-          quantity,
-          category,
-          color,
-          size,
-        },
-        { new: true }
-      );
-
-      res.status(200).json(updatedProduct);
-    }
-  } catch (error) {
-    console.error("Error updating portfolio item:", error);
-    res.status(500).json({ error: "Internal Server Error" });
   }
-});
+);
 
 // Get products
 router.get("/", async (req, res) => {
@@ -174,10 +185,8 @@ router.get("/", async (req, res) => {
 
 //Find by id
 router.get("/:id", async (req, res) => {
-  console.log("Hellow");
   try {
     const productbyId = await product.findById(req.params.id);
-
     res.status(200).json(productbyId);
   } catch (error) {
     res.status(500).json(error);
@@ -185,7 +194,7 @@ router.get("/:id", async (req, res) => {
 });
 
 // Delete
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
   try {
     await product.findByIdAndDelete(req.params.id);
     res.status(200).json("Product has been deleted...");
